8a6ca9f30189f4b981e06cb272b5542a9ba0a774
EddyBautista <bautista.j.eddy@gmail.com>
2022-03-23 18:30:52 -0500

    add notes for web api controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,11 +4,25 @@ namespace dotnet_6_webapi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+
+// ApiController enables behaviors that make it easier to build api
+// Note - Controller class in mvc is not the same as controller base. 
+// - Controller - Web Pages 
+// - ControllerBase - Web API
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing",
+        "Bracing", 
+        "Chilly", 
+        "Cool", 
+        "Mild", 
+        "Warm", 
+        "Balmy", 
+        "Hot", 
+        "Sweltering", 
+        "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
@@ -17,7 +31,7 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
-
+    // routes the get request to the public enum GET()
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
