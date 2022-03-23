a69a03594f03f8aa179be2c926fe39abad2865a5
EddyBautista <bautista.j.eddy@gmail.com>
2022-03-23 18:33:09 -0500

    add pizza class

---
+++ Models/Pizza.cs
@@ -0,0 +1,10 @@
+namespace dotnet_6_webapi.Models
+{
+    public class Pizza
+    {
+       public int Id { get; set; }
+       public string? Name { get; set; }
+       public bool IsGlutenFree { get; set; }
+        
+    }
+}
